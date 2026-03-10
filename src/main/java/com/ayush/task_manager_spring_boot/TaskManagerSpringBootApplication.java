693bcf062bb909f5f40fc85dc21661867c70d0df
ayush-anilan <ayushanilan369@gmail.com>
2026-03-10 13:28:22 +0530

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/ayush/task_manager_spring_boot/TaskManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ayush.task_manager_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayush/task_manager_spring_boot/TaskManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.task_manager_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
